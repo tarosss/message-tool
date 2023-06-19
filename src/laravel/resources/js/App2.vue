7b47c65a7175aa66c4334a0a1637cbb52f66dae4
tarosss <fdbeesk@gmail.com>
2023-06-19 21:52:48 +0900

    piniaとvue-routerのインストール

--- src/laravel/resources/js/App2.vue
+++ src/laravel/resources/js/App2.vue
@@ -1,27 +1,12 @@
 <template>
     <div>    
-        <span >変更 {{ p }}</span>
-        <p>
-            {{ x }}
-        </p>
-        <p>
-        </p>
-        <p>
-            {{ y }}
-        </p>
+        App2ca,a
     </div>
 </template>
 <script setup lang="ts">
 import { directive } from "@babel/types";
 import { computed, defineComponent, ref, onMounted, onUnmounted, watch, watchEffect } from "vue";
-import { useStore } from 'vuex'
-import {useMouse} from './mouse'
-const app2 = 'app2'
-const store = useStore()
-const p = 1
 
-const {x,y} = useMouse()
-console.log(x.value,y.value);
 
 // watchEffect(() => {
 //     const temp = x.value
---
+++ src/laravel/resources/js/Route.vue
@@ -0,0 +1,10 @@
+<template>
+    Route
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">Edit</router-link>
+    <router-view />
+</template>
+
+<script setup lang="ts">
+
+</script>
