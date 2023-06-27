f79faae75592301eee1d8affba85ee24b9716741
tarosss <fdbeesk@gmail.com>
2023-06-27 22:30:26 +0900

    メッセージ登録を複数できるように変更

--- src/laravel/app/Http/Controllers/Api/MessageController.php
+++ src/laravel/app/Http/Controllers/Api/MessageController.php
@@ -45,31 +45,39 @@ class MessageController extends \App\Http\Controllers\Controller
         try {
             $storage = \App\Factories\StorageFactory::getStorage();
 
-            $insertedMessage = $messageToolRepository->createMessage([
-                'message' => $request->input('message'),
-                'member_id' => $request->input('member_id'),
-                'storage' => $storage->getDisk()
-            ]);
+            $now = Date::getNow();
+
+            foreach($request['data'] as $data) {
+                Log::info($data);
+                $insertedMessage = $messageToolRepository->createMessage([
+                    'message' => $data['message'],
+                    'member_id' => $data['member_id'],
+                    'storage' => $storage->getDisk(),
+                    'sended' => $data['sended']
+                ]);
+
+                $insertFiles = [];
+                foreach($data['files'] as $file) {
+                    $fileName = (function () use ($file) {
+                        $fileName = StringUtils::getRandomString(30);
+                        $extention = MimeType::getExtentionFromMimeType($file->getMimeType());
+                        return $fileName . '.' . $extention;
+                    })();
+                    // ファイルを保存する
+                    $storage->putFileAs('message', $file, $fileName);
+                    
+                    $insertFiles[] = [
+                        'message_id' => $insertedMessage->_id,
+                        'original_file_name' => $file->getClientOriginalName(),
+                        'file_name' => $fileName,
+                        'created_at' => $now,
+                    ];
+                }
+            }
+
 
             // ファイルの保存
-            $now = Date::getNow();
-            $insertFiles = [];
-            foreach($request['files'] as $file) {
-                $fileName = (function () use ($file) {
-                    $fileName = StringUtils::getRandomString(30);
-                    $extention = MimeType::getExtentionFromMimeType($file->getMimeType());
-                    return $fileName . '.' . $extention;
-                })();
-                // ファイルを保存する
-                $storage->putFileAs('message', $file, $fileName);
-                
-                $insertFiles[] = [
-                    'message_id' => $insertedMessage->_id,
-                    'original_file_name' => $file->getClientOriginalName(),
-                    'file_name' => $fileName,
-                    'created_at' => $now,
-                ];
-            }
+
 
             // テキストベースのデータ
             $messageToolRepository->createFiles($insertFiles);
