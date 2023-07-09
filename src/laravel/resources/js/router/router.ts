3efcac4063696829634d02f8b3c8aef6be89b6e4
tarosss <fdbeesk@gmail.com>
2023-07-09 17:39:25 +0900

    ヘッダのデザイン作成

---
+++ src/laravel/resources/js/router/router.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Message from './Message.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: Message,
+  },
+  {
+    path: '/about',
+    component: Message,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
+})
+
+export default router
