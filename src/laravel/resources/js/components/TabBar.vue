a32b9f216be7ede256eed35a8a991111cb355157
tarosss <fdbeesk@gmail.com>
2023-08-24 22:39:28 +0900

    reaction-barを修正

---
+++ src/laravel/resources/js/components/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <q-tabs
+        v-model="tab"
+        class="bg-teal text-yellow shadow-2">
+
+        <q-tab 
+            v-for="tab of props.tabs" 
+            :key="props.message._id + tab.name"
+            :name="tab.name" 
+            :icon="tab.icon" >
+        </q-tab>
+    </q-tabs>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+const props = defineProps<{
+    message: Message,
+    tabs: {
+        name: string,
+        icon: string,
+    }[]
+}>()
+const tab = ref('')
+
+</script>
